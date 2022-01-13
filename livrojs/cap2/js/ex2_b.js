ea7f628b8e0c74c247dc4263a17853d2ffedb7c6
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-12 23:54:07 -0300

    ex2b

--- livrojs/cap2/js/ex2_b.js
+++ livrojs/cap2/js/ex2_b.js
@@ -1,8 +1,8 @@
-function calcularValor() {
+function calcularLanHouse() {
     // cria referência aos elementos da página
     var inValor = document.getElementById("inValor")
     var inTempo = document.getElementById("inTempo")
-    var outPreco = document.getElementById("outPreco")
+    var outPagar = document.getElementById("outPagar")
 
     // obtém conteúdo dos campos de entrada
     var valor = Number(inValor.value)
@@ -11,9 +11,9 @@ function calcularValor() {
     // Calcula valor a ser pago por 15 min
     var preco = Math.ceil(tempo / 15) * valor
     // altera o conteúdo da linha de resposta
-    outPreco.textContent = "Valor a pagar R$: " + preco.toFixed(2)
+    outPagar.textContent = "Valor a pagar R$: " + preco.toFixed(2)
 }
 // cria referência ao elemento btCalcular 
 var btCalcular = document.getElementById("btCalcular")
 // registra um evento associado ao botão, para carregar a função
-btCalcular.addEventListener("click", calcularValor)
+btCalcular.addEventListener("click", calcularLanHouse)
