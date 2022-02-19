d86291a941370f80ba5eb856b593d8f1c7a4910f
Charles Pereira <charlespereira.fln@gmail.com>
2022-02-19 00:37:42 -0300

    ex4-2

--- livrojs/cap4/js/ex4_2.js
+++ livrojs/cap4/js/ex4_2.js
@@ -15,12 +15,17 @@ function listarNumeros() {
     // inicializa variável resposta
     var resposta = "Entre " + numero + " e 1: "
 
-    // cria um for decrescente
+   /* // cria um for decrescente
     for (var i = numero; i > 0; i = i - 1) {
         // resposta vai acumulando números (e virgulas)
         resposta = resposta + i + ", "
     }
-
+    */
+    // Solução para tirar a vírgula do final.
+    for (var i = numero; i > 1; i--) {
+        resposta = resposta + i + ", "
+    }
+    resposta = resposta + i + "."
     // altera o conteúdo de outResposta
     outResposta.textContent = resposta
 }
